282df6ad0deaff2beea31378e323807effc6e8ce
sbugniht <sbugni24@gmail.com>
2025-09-21 11:39:25 +0200

    .

--- table.sql
+++ table.sql
@@ -11,27 +11,36 @@ CREATE TABLE Airport(
 );
 
 CREATE TABLE Visitor(
-USER_ID INT,
-PRIMARY KEY (USER_ID),
+    USER_ID INT,
+    PRIMARY KEY (USER_ID),
+
+);
+
+CREATE TABLE login(
+    USER_ID INT NOT NULL,
+    pwd INT,
+    FOREIGN KEY (USER_ID) REFERENCES Visitor(USER_ID),
 
 );
 
 CREATE TABLE Logged_user(
-USER_ID INT,
-PRIMARY KEY (USER_ID),
+    USER_ID INT,
+    pwd INT, 
+    PRIMARY KEY (USER_ID),
 );
 
-CREATE TABLE Admin(
-USER_ID INT,
-PRIMARY KEY (USER_ID),
+    CREATE TABLE Admin(
+    USER_ID INT,
+    PRIMARY KEY (USER_ID),
 );
 
 CREATE TABLE Flight(
-flight_id INT,
-airport_id INT,
-plane_id INT,
-
-PRIMARY KEY (flight_id),
-FOREIGN KEY (plane_id) REFERENCES Plane(plane_id),
-FOREIGN KEY (airport_id) REFERENCES Airport(airport_id)
+    flight_id INT,
+    airport_id INT,
+    plane_id INT,
+    plane_status CHAR,
+    
+    PRIMARY KEY (flight_id),
+    FOREIGN KEY (plane_id) REFERENCES Plane(plane_id),
+    FOREIGN KEY (airport_id) REFERENCES Airport(airport_id)
 );
