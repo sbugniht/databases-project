30386c2c34fd94b3b19704c481ab540bdb35de84
sbugniht <sbugni24@gmail.com>
2025-09-28 20:58:34 +0200

    populationTest

--- population.sql
+++ population.sql
@@ -1,14 +1,16 @@
 -- Insert rows into table 'Plane'
-INSERT INTO Plane
-( -- columns to insert data into
- [plane_id] 
-)
+INSERT INTO Plane( plane_id)  -- columns to insert data into
+
 VALUES
 ( -- first row: values for the columns in the list above
- 1
+    1
 ),
 ( -- second row: values for the columns in the list above
- 2
+    2
+),
+(
+    3
 )
 -- add more rows here
+
 GO
