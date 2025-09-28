fc57795d31c20d43351ea7c3b3a37b5f88a93c5f
sbugniht <sbugni24@gmail.com>
2025-09-28 21:01:00 +0200

    popTest2

--- population.sql
+++ population.sql
@@ -1,16 +1,9 @@
 -- Insert rows into table 'Plane'
-INSERT INTO Plane( plane_id)  -- columns to insert data into
+INSERT INTO Plane (plane_id)  -- columns to insert data into
 
 VALUES
-( -- first row: values for the columns in the list above
-    1
-),
-( -- second row: values for the columns in the list above
-    2
-),
-(
-    3
-)
+    (1),  -- first row: values for the columns in the list above
+    (2), -- second row: values for the columns in the list above
+    (3);
 -- add more rows here
 
-GO
