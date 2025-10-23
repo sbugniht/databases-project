d1f0830a9bf4a2cacb0b75b1403e311c8e018e96
sbugniht <sbugni24@gmail.com>
2025-10-23 14:33:00 +0200

    14th

--- public_html/admin.php
+++ public_html/admin.php
@@ -139,7 +139,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard - SkyBook</title>
-    <link rel="admin_style" href="admin-style.css">
+    <!-- <link rel="admin_style" href="admin-style.css"> -->
     <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
